1874ce0f4d1720eb16ab7571799e23fde576f7b6
FernandoStentzler <stentzlerfer@hotmail.com>
2022-05-10 14:03:55 -0300

    Adicionando o Multer para Salvar Imagens

--- src/routes/filmes.js
+++ src/routes/filmes.js
@@ -1,7 +1,21 @@
 const express = require('express');
 const router = express.Router();
+const path = require('path')
 const filmesController = require('../controllers/filmesController');
 const multer = require('multer')
+
+const multerStorage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        const folder = path.join(__dirname, "../../public/images/my-uploads");
+        cb(null, folder);
+    },
+    filename: function (req, file, cb) {
+      const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9);
+      cb(null, file.fieldname + '-' + uniqueSuffix + '.jpg');
+    },
+  });
+  
+  const upload = multer({ storage: multerStorage });
 
 // Listagem De Filmes
 router.get("/", filmesController.todosFilmes)
@@ -10,7 +24,7 @@ router.get("/", filmesController.todosFilmes)
 router.get("/:id", filmesController.filmesPorId)
 
 //Criação de Filmes
-router.post("/", filmesController.criacaoDeFilmes)
+router.post("/",upload.single('avatar') ,filmesController.criacaoDeFilmes)
 
 //Atualização de Filmes
 router.put('/:id', filmesController.atualizarFilmes)
